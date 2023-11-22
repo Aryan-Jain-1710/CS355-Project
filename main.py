caf684254fee0b13e6a21359717c4f01043251ef
Shreya Ompreeti <64154431+shreya-o@users.noreply.github.com>
2023-11-22 12:12:26 -0800

    basic sha256 implementation

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-import socket
+import sys
+import hashlib
 
-def main():
-    print("hello")
+def hashfile(file):
 
-if __name__ == "__main__":
-    main()
+	# Arbitrary buffer size of 64 kb to read files
+	BUF_SIZE = 65536
 
+	sha256 = hashlib.sha256()
+
+	with open(file, 'rb') as f:
+		while True:
+			data = f.read(BUF_SIZE)
+
+			# data = 0 when we reach EOF
+			if not data:
+				break
+	
+			# Updating the SHA256 hash of the file
+			sha256.update(data)
+
+	# hexdigest() gives a hexadecimal representation of the hashed value of 
+    # all the data passed to the sha256 function with update()
+	return sha256.hexdigest()
+
+f1_hash = hashfile(sys.argv[1])
+print(f"Hash: {f1_hash}")
 
 
